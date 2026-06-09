77cffba242ffe2d0a856eb1e68d4d81621c04756
Danirodriguez45B <danielrodriguez45b@gmail.com>
2026-06-09 12:06:42 -0300

    update modelo usuario

---
+++ models/usuario.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Usuario = sequelize.define('Usuario', {
+  username: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false }
+}, {
+  tableName: 'usuarios',
+  timestamps: false
+});
+
+module.exports = Usuario;
