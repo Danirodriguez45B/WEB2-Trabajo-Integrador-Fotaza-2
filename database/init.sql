1e91fd965f56a6d94f6d42782b9d20b9eb474d3e
Danirodriguez45B <danielrodriguez45b@gmail.com>
2026-06-11 00:50:25 -0300

    update init

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,8 @@
-DROP TABLE IF EXISTS usuarios CASCADE;
-DROP TABLE IF EXISTS publicaciones CASCADE;
+DROP TABLE IF EXISTS valoraciones CASCADE;
+DROP TABLE IF EXISTS comentarios CASCADE;
 DROP TABLE IF EXISTS imagenes CASCADE;
+DROP TABLE IF EXISTS publicaciones CASCADE;
+DROP TABLE IF EXISTS usuarios CASCADE;
 
 CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
@@ -27,3 +29,26 @@ CREATE TABLE imagenes (
     licencia VARCHAR(50) NOT NULL,
     CONSTRAINT fk_publicacion FOREIGN KEY (publicacion_id) REFERENCES publicaciones(id) ON DELETE CASCADE
 );
+
+CREATE TABLE comentarios (
+    id SERIAL PRIMARY KEY,
+    publicacion_id INT NOT NULL,
+    usuario_id INT NOT NULL,
+    contenido TEXT NOT NULL,
+    CONSTRAINT fk_publicacion_comentario FOREIGN KEY (publicacion_id) REFERENCES publicaciones(id) ON DELETE CASCADE,
+    CONSTRAINT fk_usuario_comentario FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+CREATE TABLE valoraciones (
+    id SERIAL PRIMARY KEY,
+    publicacion_id INT NOT NULL,
+    usuario_id INT NOT NULL,
+    voto INT NOT NULL,
+    CONSTRAINT voto_unico UNIQUE (publicacion_id, usuario_id),
+    CONSTRAINT fk_publicacion_voto FOREIGN KEY (publicacion_id) REFERENCES publicaciones(id) ON DELETE CASCADE,
+    CONSTRAINT fk_usuario_voto FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+INSERT INTO usuarios (username, email, password, rol, estado) VALUES
+('admin_test', 'admin@correo.com', 'admin123', 'validador', 'activo'),
+('juan_perez', 'juan@correo.com', 'user123', 'usuario', 'activo');
