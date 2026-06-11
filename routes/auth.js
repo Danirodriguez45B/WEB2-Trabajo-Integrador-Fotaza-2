b17eee41daee40357b6d35cba56e799923662324
Danirodriguez45B <danielrodriguez45b@gmail.com>
2026-06-10 21:58:22 -0300

    update login

--- routes/auth.js
+++ routes/auth.js
@@ -6,3 +6,9 @@ const authController = require('../controllers/authController');
 // Rutas de Registro
 router.get('/registro', authController.mostrarRegistro);
 router.post('/registro', authController.registrarUsuario);
+
+// Rutas de Login
+router.get('/login', authController.mostrarLogin);
+router.post('/login', authController.loginUsuario);
+router.get('/logout', authController.logoutUsuario);
+module.exports = router;
