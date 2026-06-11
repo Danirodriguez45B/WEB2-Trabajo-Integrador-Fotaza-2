a8f279cbeec0c259f17f8025fb6e069725b457a5
Danirodriguez45B <danielrodriguez45b@gmail.com>
2026-06-10 21:10:59 -0300

    definicion de relaciones

--- models/index.js
+++ models/index.js
@@ -6,6 +6,35 @@ const Comentario = require('./comentario');
 const Valoracion = require('./valoracion');
 const Seguidor = require('./seguidor');
 
+// aca defino las relaciones 
+Usuario.hasMany(Publicacion, { foreignKey: 'usuarioId' });
+Publicacion.belongsTo(Usuario, { foreignKey: 'usuarioId' });
+
+Publicacion.hasMany(Archivo, { foreignKey: 'publicacionId', as: 'Archivos' });
+Archivo.belongsTo(Publicacion, { foreignKey: 'publicacionId' });
+
+Publicacion.hasMany(Comentario, { foreignKey: 'publicacionId', as: 'Comentarios' });
+Comentario.belongsTo(Publicacion, { foreignKey: 'publicacionId' });
+
+Usuario.hasMany(Comentario, { foreignKey: 'usuarioId' });
+Comentario.belongsTo(Usuario, { foreignKey: 'usuarioId' });
+
+Publicacion.hasMany(Valoracion, { foreignKey: 'publicacionId', as: 'Valoraciones' });
+Valoracion.belongsTo(Publicacion, { foreignKey: 'publicacionId' });
+
+Usuario.belongsToMany(Usuario, { 
+  as: 'Seguidores', 
+  through: Seguidor,
+  foreignKey: 'seguidoId', 
+  otherKey: 'seguidorId' 
+});
+
+Usuario.belongsToMany(Usuario, { 
+  as: 'Siguiendo', 
+  through: Seguidor, 
+  foreignKey: 'seguidorId', 
+  otherKey: 'seguidoId' 
+});
 
 module.exports = {
   sequelize,
